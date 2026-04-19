641a280e3188ea679a44b68bec661407cc585630
Nitish Kopparaju <nitishkopparaju@gmail.com>
2026-04-19 21:51:05 +0530

    Day 1 2nd Problem

---
+++ 01_Arrays/sumofarr.py
@@ -0,0 +1,15 @@
+'''Find the sum of all elements in an array.
+Example
+a = [1, 2, 3, 4, 5]
+The sum of the elements is 1 + 2 + 3 + 4 + 5 = 15
+Function Description
+Complete the simpleArraySum function in the editor below.
+simpleArraySum has the following parameter(s):
+int ar[n]: an array of integers
+Returns
+'''
+ar=[-1, 0 ,1]
+total=0
+for i in ar:
+  total+=i
+print(total)
