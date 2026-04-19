3e4995f01a06563daa0210cf6efb729de5e8c637
Nitish Kopparaju <nitishkopparaju@gmail.com>
2026-04-19 22:00:57 +0530

    Day 1 2nd Problem with changes

--- 01_Arrays/sumofarr.py
+++ 01_Arrays/sumofarr.py
@@ -8,8 +8,23 @@ simpleArraySum has the following parameter(s):
 int ar[n]: an array of integers
 Returns
 '''
-ar=[-1, 0 ,1]
-total=0
-for i in ar:
-  total+=i
 print(total)
+def aVeryBigSum(ar):
+    """
+    Calculate the sum of very large integers in an array.
+
+    Example Input:
+    ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+
+    Expected Output:
+    5000000015
+    """
+    total=0
+    for i in ar:
+        total+=i
+    return total
+
+
+# Sample test case
+ar = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+print(aVeryBigSum(ar))
